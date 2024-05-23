cc41f97d606a64b390973487d2c7a0679f6d40a2
GiaKhangLuu <waoknight@gmail.com>
2024-05-23 17:43:41 +0700

    backup

--- two_cam_two_view.py
+++ two_cam_two_view.py
@@ -10,6 +10,29 @@ import queue
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 
+def unnormalize_landmarks(landmarks, window_size):
+    unnorm_landmarks = landmarks[:, :-1]
+    unnorm_landmarks[:, 0] = unnorm_landmarks[:, 0] * window_size[0]
+    unnorm_landmarks[:, 1] = unnorm_landmarks[:, 1] * window_size[1]
+
+    """
+    Note: some landmarks have value > or < window_height and window_width,
+        so this will cause the depth_image out of bound. For now, we just 
+        clip in in the range of window's dimension. But those values 
+        properly be removed from the list.
+    """
+
+    unnorm_landmarks[:, 0] = np.clip(unnorm_landmarks[:, 0], 0, window_size[0] - 1)
+    unnorm_landmarks[:, 1] = np.clip(unnorm_landmarks[:, 1], 0, window_size[1] - 1)
+
+    return unnorm_landmarks
+
+def get_depth(xy_landmarks, depth_image):
+    xy_landmarks = xy_landmarks.astype(np.int32)
+    z_landmarks = depth_image[xy_landmarks[:, 1], xy_landmarks[:, 0]]
+    
+    return z_landmarks
+
 # RealSense processing function
 def process_realsense(rs_queue):
     hands = mp_hands.Hands(max_num_hands=2, min_detection_confidence=0.5)
@@ -18,6 +41,7 @@ def process_realsense(rs_queue):
     config_rs.enable_stream(rs.stream.color, 640, 360, rs.format.bgr8, 30)
     config_rs.enable_stream(rs.stream.depth, 640, 360, rs.format.z16, 30)
     pipeline_rs.start(config_rs)
+    window_size = (640, 360)
 
     while True:
         frames = pipeline_rs.wait_for_frames()
@@ -35,8 +59,26 @@ def process_realsense(rs_queue):
         frame_rs_rgb = cv2.cvtColor(frame_rs, cv2.COLOR_BGR2RGB)
         results_rs = hands.process(frame_rs_rgb)
         if results_rs.multi_hand_landmarks:
-            for hand_landmarks in results_rs.multi_hand_landmarks:
+            for hand_num, hand_landmarks in enumerate(results_rs.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(frame_rs, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+                raw_landmarks = []
+                for landmark in hand_landmarks.landmark:
+                    x = landmark.x 
+                    y = landmark.y
+                    z = landmark.z
+                    raw_landmarks.append([x, y, z]) 
+                raw_landmarks = np.array(raw_landmarks)
+
+                #print("index finger raw: ", np.around(raw_landmarks[5:9, :], decimals=2))
+
+                landmarks_xy_unnorm = unnormalize_landmarks(raw_landmarks, window_size)
+
+                landmarks_Z = get_depth(landmarks_xy_unnorm, depth_rs)
+
+                landmarks_xyZ = np.concatenate([landmarks_xy_unnorm, landmarks_Z[:, None]], axis=-1)
+
+                #print("RealSense xyz (hand {}): ".format(hand_num), landmarks_xyZ)
 
         rs_combined = np.hstack((frame_rs, depth_rs_display))
         rs_queue.put(rs_combined)
@@ -57,6 +99,9 @@ def process_oak(oak_queue):
     mono_left = pipeline_oak.create(dai.node.MonoCamera)
     mono_right = pipeline_oak.create(dai.node.MonoCamera)
     stereo = pipeline_oak.create(dai.node.StereoDepth)
+
+    stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+    stereo.setOutputSize(640, 360)
 
     mono_left.setBoardSocket(dai.CameraBoardSocket.LEFT)
     mono_right.setBoardSocket(dai.CameraBoardSocket.RIGHT)
@@ -79,6 +124,8 @@ def process_oak(oak_queue):
     rgb_queue_oak = device_oak.getOutputQueue(name="rgb", maxSize=4, blocking=False)
     depth_queue_oak = device_oak.getOutputQueue(name="depth", maxSize=4, blocking=False)
 
+    window_size = (640, 360)
+
     while True:
         rgb_frame_oak = rgb_queue_oak.get()
         depth_frame_oak = depth_queue_oak.get()
@@ -86,8 +133,8 @@ def process_oak(oak_queue):
         frame_oak = rgb_frame_oak.getCvFrame()
         depth_oak = depth_frame_oak.getFrame()
 
-        frame_oak = cv2.resize(frame_oak, (640, 360))
-        #depth_oak_display = cv2.resize(depth_oak, (640, 480))
+        frame_oak = cv2.resize(frame_oak, window_size)
+        #depth_oak = cv2.resize(depth_oak, window_size)
 
         depth_oak_display = cv2.normalize(depth_oak, None, 0, 255, cv2.NORM_MINMAX)
         depth_oak_display = cv2.applyColorMap(depth_oak_display.astype(np.uint8), cv2.COLORMAP_JET)
@@ -95,11 +142,29 @@ def process_oak(oak_queue):
         frame_oak_rgb = cv2.cvtColor(frame_oak, cv2.COLOR_BGR2RGB)
         results_oak = hands.process(frame_oak_rgb)
         if results_oak.multi_hand_landmarks:
-            for hand_landmarks in results_oak.multi_hand_landmarks:
+            for hand_num, hand_landmarks in enumerate(results_oak.multi_hand_landmarks):
                 mp_drawing.draw_landmarks(frame_oak, hand_landmarks, mp_hands.HAND_CONNECTIONS)
 
-        #oak_combined = np.hstack((frame_oak, depth_oak_display))
-        oak_queue.put(frame_oak)
+                raw_landmarks = []
+                for landmark in hand_landmarks.landmark:
+                    x = landmark.x 
+                    y = landmark.y
+                    z = landmark.z
+                    raw_landmarks.append([x, y, z]) 
+                raw_landmarks = np.array(raw_landmarks)
+
+                #print("index finger raw: ", np.around(raw_landmarks[5:9, :], decimals=2))
+
+                landmarks_xy_unnorm = unnormalize_landmarks(raw_landmarks, window_size)
+
+                landmarks_Z = get_depth(landmarks_xy_unnorm, depth_oak)
+
+                landmarks_xyZ = np.concatenate([landmarks_xy_unnorm, landmarks_Z[:, None]], axis=-1)
+
+                print("OAK xyz (hand {}): ".format(hand_num), landmarks_xyZ)
+
+        oak_combined = np.hstack((frame_oak, depth_oak_display))
+        oak_queue.put(oak_combined)
 
         if oak_queue.qsize() > 1:
             oak_queue.get()
