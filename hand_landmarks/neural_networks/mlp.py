e409f496ed46c169e7cdaae09fdc639de8e8fdac
GiaKhangLuu <waoknight@gmail.com>
2024-06-16 17:01:09 +0700

    backup from personal laptop

--- hand_landmarks/neural_networks/mlp.py
+++ hand_landmarks/neural_networks/mlp.py
@@ -5,14 +5,20 @@ class MLP(nn.Module):
     def __init__(self, input_size=21*3*2, output_size=21*3):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_size, 100)
-        self.fc2 = nn.Linear(100, 64)
-        self.fc3 = nn.Linear(64, 64)
-        self.fc4 = nn.Linear(64, output_size)
+        self.fc2 = nn.Linear(100, 100)
+        self.fc3 = nn.Linear(100, 100)
+        self.fc4 = nn.Linear(100, 64)
+        self.fc5 = nn.Linear(64, 64)
+        self.fc6 = nn.Linear(64, output_size)
+        self.tanh = nn.Tanh()
+        self.sigmoid = nn.Sigmoid()
         self.relu = nn.ReLU()
         
     def forward(self, x):
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
-        x = self.fc4(x)
+        x = self.relu(self.fc4(x))
+        x = self.relu(self.fc5(x))
+        x = self.fc6(x)
         return x
